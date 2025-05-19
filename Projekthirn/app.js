a761f63f07b43f4e0dd1ba6395e9c4bf8b211e81
200326 <200326@studierende.htl-donaustadt.at>
2025-05-19 13:54:43 +0200

    ohne frontend

--- Projekthirn/app.js
+++ Projekthirn/app.js
@@ -14,5 +14,3 @@ backend(app);
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server läuft unter http://localhost:${port}`);
 });
-
-// ich will es aber so machen, das wenn man den Code sieht, dass man einfach darauf kommt, dass ich es war, indem man Methoden verwendet, die wir regelmäßig im Unterricht verwendeten oder so.
