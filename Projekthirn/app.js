733c3dbdd73c86ee3b43dcfce9e5878d56911bfc
Benjaminibi <200326@studierende.htl-donaustadt.at>
2025-06-03 22:34:49 +0200

    Kommentare hinzugefügt

--- Projekthirn/app.js
+++ Projekthirn/app.js
@@ -10,10 +10,10 @@ app.use(cors());
 
 app.use(express.json());
 
-
+    //backend wird einfach aufgerufen
 backend(app);
 
-
+    //hier wird der Server gestartet und der Hostname von 0.0.0.0 erlaubt es für jede IP Adresse das Backend zu verwenden
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server läuft unter http://localhost:${port}`);
 });
--- Projekthirn/backend.js
+++ Projekthirn/backend.js
@@ -13,13 +13,14 @@ let pdfLinks = [];
 }
 
 const backend = (app) => {
-
+        //statische Verwendung der PDFs
     app.use('/backend_asset', express.static(path.join(__dirname, 'public', 'backend_asset')));
 
-
+        // Dies ist die dynamische Routing Methode, die verwendet wird, wobei "lappen" der Platzhalter ist
     app.get('/:lappen', (req, res) => {
+            //hier wird der eingegebene Pfad angenommen und automatisch auf kleingeschrieben gesetzt, damit keine Fehler durch groß- und Kleinschreibung entstehen
         const requestedLobe = req.params.lappen.toLowerCase();
-
+            //Jetzt wird ein Platzhalter "found" mittels Methode .find() von der PDFLinks.json Datei gefüllt und überprüft, ob .title mit dem eingegeben Titel übereinstimmt.
         const found = pdfLinks.find(entry =>
             entry.title.toLowerCase() === requestedLobe
         );
@@ -27,7 +28,7 @@ const backend = (app) => {
         if (!found) {
             return res.status(404).json({ error: 'PDF nicht gefunden' });
         }
-
+            //Dies ist dann die Ausgabe, id, Titel und URL wird aanggezeiigt je nach dem eingegebenen Titel
         res.json({
             id: found.id,
             name: found.title,
